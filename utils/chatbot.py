8e33dd414718799291cf8b027e18e4be74099a75
samurai-py <marioestevao@gmail.com>
2024-03-14 12:53:54 -0300

    uploading new version with sabia7b

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -6,21 +6,23 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.llms import HuggingFaceHub
 
 def create_vectorstore(chunks):
-    embeddings = HuggingFaceInstructEmbeddings(model_name="WhereIsAI/UAE-Large-V1")
+    embeddings = HuggingFaceInstructEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
     new_vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)    
+    
+    #vectorstore.save_local('faiss_default')
 
-    vectorsetore = FAISS.load_local('faiss_default')
-    vectorsetore.merge_from(new_vectorstore)
+    vectorstore = FAISS.load_local('faiss_default', embeddings=embeddings, allow_dangerous_deserialization=True)
+    vectorstore.merge_from(new_vectorstore)
     
     return vectorstore
 
 def create_conversation_chain(vectorstore=None):
     
     if(not vectorstore):
-        embeddings = HuggingFaceInstructEmbeddings(model_name="WhereIsAI/UAE-Large-V1")
-        vectorstore = FAISS.load_local('faiss_default', embeddings, allow_dangerous_deserialization=True)
+        embeddings = HuggingFaceInstructEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+        vectorstore = FAISS.load_local('faiss_default', embeddings=embeddings, allow_dangerous_deserialization=True)
     
-    llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":0.5, "max_length":512})
+    llm = HuggingFaceHub(repo_id="maritaca-ai/sabia-7b", model_kwargs={"temperature":0.5, "max_length":512})
     
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     
